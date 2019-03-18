45b50374ebc4b19fd354967f5931974c9db6349e
Frederick Cai <frederick.cai@gmail.com>
2019-03-19 11:31:30 +1300

    upgrade to @nestjs/graphql v6.0.1

--- test/resolver-without-type-name.spec.ts
+++ test/resolver-without-type-name.spec.ts
@@ -1,27 +1,23 @@
-import { Resolver, Query } from '@nestjs/graphql';
 import { buildSchema } from 'type-graphql';
+
+import { Resolver, Query } from '@nestjs/graphql';
+
+/**
+ * Switch to this import so that you can see the expected output.
+ */
+// import { Resolver, Query } from 'type-graphql';
+
+import 'reflect-metadata';
 
 /*
   Test output:
   ============
 
-  TypeError: Cannot read property 'prototype' of undefined
+  Generating schema error
 
-    2 | import { Resolver, Query } from '@nestjs/graphql';
-    3 | import { buildSchema } from 'type-graphql';
-  > 4 |
-      | ^
-    5 | describe('Issue #1', () => {
-    6 |
+  at Function.<anonymous> (node_modules/type-graphql/dist/schema/schema-generator.js:19:23)
+  at fulfilled (node_modules/tslib/tslib.js:104:62)
 
-    at isConstructor (node_modules/@nestjs/graphql/dist/decorators/resolvers.decorators.js:100:19)
-    at getClassName (node_modules/@nestjs/graphql/dist/decorators/resolvers.decorators.js:93:12)
-    at Resolver (node_modules/@nestjs/graphql/dist/decorators/resolvers.decorators.js:80:22)
-    at DecorateConstructor (node_modules/reflect-metadata/Reflect.js:541:33)
-    at Object.decorate (node_modules/reflect-metadata/Reflect.js:130:24)
-    at Object.<anonymous>.__decorate (test/resolver-without-type-name.spec.ts:4:92)
-    at Object.<anonymous> (test/resolver-without-type-name.spec.ts:11:25)
-    at test/resolver-without-type-name.spec.ts:13:71
 */
 it('should support @Resolver without specifying type name', async () => {
   @Resolver(/* No Type Name or Type Class */)
